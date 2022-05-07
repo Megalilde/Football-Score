d3c9919e35b19eb52cf64bd54927b7dc761f1756
Megalilde <63415418+Megalilde@users.noreply.github.com>
2022-05-07 17:10:25 +0200

    created base layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:football_score/utils/constants.dart';
 
 void main() {
   runApp(const FootballHome());
@@ -9,6 +10,50 @@ class FootballHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Color(safeAreaColor),
+          appBar: AppBar(
+            backgroundColor: Color(appbarColor),
+            title: Center(child: Text('Home')),
+            ),
+          body: ListView(
+            padding: EdgeInsets.all(16.0),
+            children: [
+              Container(
+                height: 50,
+                color: Colors.amber[600],
+                child: const Center(child: Text('Entry A')),
+              ),
+              Container(
+                height: 50,
+                color: Colors.amber[500],
+                child: const Center(child: Text('Entry B')),
+              ),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.alternate_email),
+                    label: 'Partite',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.web_outlined),
+                    label: 'Shop',
+                  ),
+                ],
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Colors.red.shade800,
+            backgroundColor: Color(appbarColor),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+
+
+const safeAreaColor = 0xFF0B0B0B;
+const appbarColor = 0xFF1B1B26;
